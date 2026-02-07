6c50f21b3da427f9a14db92978634e3cbbfa4972
Rohit Tomar <tomarrohit5034@gmail.com>
2026-02-07 21:49:52 +0530

    tailwind dependency changed

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
+
+export default config;
