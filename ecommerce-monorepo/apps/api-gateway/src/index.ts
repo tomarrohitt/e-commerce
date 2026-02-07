2fb25121ee25262d2b7159ed22e7e185755ba400
Rohit Tomar <tomarrohit5034@gmail.com>
2026-02-08 00:40:34 +0530

    fix aws bucket name typo

--- ecommerce-monorepo/apps/api-gateway/src/index.ts
+++ ecommerce-monorepo/apps/api-gateway/src/index.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
