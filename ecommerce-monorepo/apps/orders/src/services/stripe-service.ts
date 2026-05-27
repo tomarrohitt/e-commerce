755296897ce25036348be9a44da6eb6fe83403c2
Ubuntu <tomarrohit5034@gmail.com>
2026-05-27 09:01:04 +0000

    final working config dockerfiles and yaml files for kubectl

--- ecommerce-monorepo/apps/orders/src/services/stripe-service.ts
+++ ecommerce-monorepo/apps/orders/src/services/stripe-service.ts
@@ -6,7 +6,7 @@ class StripeService {
 
   constructor() {
     this.stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-      apiVersion: "2026-01-28.clover",
+      apiVersion: "2026-02-25.clover",
       typescript: true,
     });
   }
