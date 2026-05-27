95f27c3e93821a0d4206c96d6ad65b5d450ee01c
Ubuntu <tomarrohit5034@gmail.com>
2026-05-27 05:31:07 +0000

    fix: move prisma CLI to dependencies for all services

--- ecommerce-monorepo/apps/cart/src/config/prisma.ts
+++ ecommerce-monorepo/apps/cart/src/config/prisma.ts
@@ -1,10 +1,3 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { env } from "./env";
 import { PrismaClient } from "@prisma/client";
-
-const connectionString = `${env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
+const prisma = new PrismaClient();
 export { prisma };
--- ecommerce-monorepo/apps/identity/src/config/prisma.ts
+++ ecommerce-monorepo/apps/identity/src/config/prisma.ts
@@ -1,11 +1,3 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { env } from "./env";
-
-const connectionString = `${env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
+const prisma = new PrismaClient();
 export { prisma };
